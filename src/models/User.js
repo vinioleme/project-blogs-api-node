043a0681a5794a6bfb0ce3ae376bc2514a72b991
Vinicius Leme <vinileme@gmail.com>
2023-08-06 09:02:35 -0300

    req2

---
+++ src/models/User.js
@@ -0,0 +1,30 @@
+const UserSchema = (sequelize, DataTypes) => {
+    const User = sequelize.define('User', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        displayName: DataTypes.STRING,
+        email: DataTypes.STRING,
+        password: DataTypes.STRING,
+        image: {
+            type: DataTypes.STRING,
+            defaultValue: null,
+        }
+    }, 
+    {
+        tableName: 'users',
+        underscored: true,
+        timestamps: false,
+    });
+
+    User.associate = ({ BlogPost }) => {
+        User.hasMany(BlogPost,
+          { foreignKey: 'userId', as: 'blog_posts' });
+      };
+
+    return User;
+}
+
+module.exports = UserSchema;
